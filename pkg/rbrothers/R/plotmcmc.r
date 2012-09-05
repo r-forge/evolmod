7d2d8f43d1a4e8803db72b4b30ad023c7b98f013
ji26 (Jan Irvahn) <ji26@uw.edu>
2012-09-05 20:38:19 +0000

    changed trace plot/autocorrelation plot function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/evolmod@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rbrothers/R/plotmcmc.r
+++ pkg/rbrothers/R/plotmcmc.r
@@ -1,10 +1,6 @@
-plotmcmc<-function(x,mode="trace"){
+plotmcmc<-function(x){
 library(coda)
-if(mode!="trace"&mode!="auto"){
-print("mode must be trace or auto")
-return()
-}
-par(mfrow=c(1,1))
-if(mode=="trace"){traceplot(mcmc(x$MCMC_log_likelihood),ylab="log likelihood",main="MCMC trace plot of log likelihood")}
-if(mode=="auto"){autocorr.plot(x$MCMC_log_likelihood,auto.layout=FALSE)}
+par(mfrow=c(2,1))
+traceplot(mcmc(x$MCMC_log_likelihood),ylab="log likelihood",main="MCMC trace plot of log likelihood")
+autocorr.plot(x$MCMC_log_likelihood,auto.layout=FALSE)
 }
