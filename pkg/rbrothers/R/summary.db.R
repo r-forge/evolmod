95ad8b54002a62fde1587c7cd551645596b71ac1
ji26 (Jan Irvahn) <ji26@uw.edu>
2012-11-17 00:39:08 +0000

    changed Bayes factor reporting in summary function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/evolmod@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rbrothers/R/summary.db.R
+++ pkg/rbrothers/R/summary.db.R
@@ -11,8 +11,13 @@ for(i in 1:(dim(x$command_file)[1])){
 }
 
 ### Bayes factor calculation:
-bf<-(sum(x$MCMC_number_of_break_points>0)/sum(x$MCMC_number_of_break_points==0))/(exp(top_lambda)-1)
-if(bf!=Inf) bf<-signif(bf,2)
+#bf<-(sum(x$MCMC_number_of_break_points>0)/sum(x$MCMC_number_of_break_points==0))/(exp(top_lambda)-1)
+#if(bf!=Inf) bf<-signif(bf,2)
+###
+
+### a slightly different Bayes factor calculation:
+Y<-1000*(exp(top_lambda)-1)
+rm<-Y/(1+Y)
 ###
 
 cat(paste("DualBrothers output for",x[[1]]),"\n")
@@ -28,8 +33,9 @@ cat(paste("prior mean number of substitution process change points:",par_lambda)
 cat(paste("prior mean number of topology change points:",top_lambda),"\n\n")
 
 cat(paste("average number of breakpoints in the posterior:",round(mean(x$MCMC_number_of_break_points),1)),"\n")
-if(sum(x$MCMC_number_of_break_points==0)==0){cat(paste("All of the posterior samples contained at least one break point."),"\n")}
-if(bf!=Inf){cat(paste("The Bayes factor in favor of recombination is ",bf),"\n")}
+cat(paste("The posterior probability of at least one breakpoint was",round(sum(x$MCMC_number_of_break_points>0)/length(x$MCMC_number_of_break_points),3)," (>",round(rm,3)," required for a Bayes factor > 1000).","\n"))
+#if(sum(x$MCMC_number_of_break_points==0)==0){cat(paste("All of the posterior samples contained at least one break point."),"\n")}
+#if(bf!=Inf){cat(paste("The Bayes factor in favor of recombination is ",bf),"\n")}
 if(length(x[[6]])>1){cat(paste(length(x[[6]]),"trees considered"),"\n\n")}
 if(length(x[[6]])==1){cat(paste(length(x[[6]]),"tree considered"),"\n\n")}
 
