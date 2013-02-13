789ffa987b75fc4a6a8ed18a32794d582d2c0939
vminin (Vladimir Minin) <vminin@uw.edu>
2013-02-13 18:43:55 +0000

    small fix to Salmonella script
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/evolmod@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rbrothers/demo/salmonella_example.R
+++ pkg/rbrothers/demo/salmonella_example.R
@@ -2,7 +2,7 @@ library(rbrothers)
 
 ### Salmonella fimH Analysis ########
 
-my.fimH.align = read.dna(file=system.file("extdata/fimH/Salmonella_fimH8.phy",package="rbrothers"))
+my.fimH.align = read.dna(file=system.file("extdata/Salmonella/Salmonella_fimH8.phy",package="rbrothers"))
 write.dna(my.fimH.align,"Salmonella_fimH8.phy")
 
 fimH.db<-dualbrothers(round(runif(1,1,100000),0),"Salmonella_fimH8", top_lambda=0.001, par_lambda=1, window.size=c(300,400), length=51000000, burnin=1000000, subsample=5000)
@@ -17,7 +17,7 @@ breakpointCI(fimH.db,570,700)
 
 ### Salmonella fimA Analysis ########
 
-my.fimA.align = read.dna(file=system.file("extdata/fimH/Salmonella_fimA8.phy",package="rbrothers"))
+my.fimA.align = read.dna(file=system.file("extdata/Salmonella/Salmonella_fimA8.phy",package="rbrothers"))
 write.dna(my.fimA.align,"Salmonella_fimA8.phy")
 
 fimA.db<-dualbrothers(round(runif(1,1,10000),0),"Salmonella_fimA8", top_lambda=0.001, par_lambda=1, window.size=250)
