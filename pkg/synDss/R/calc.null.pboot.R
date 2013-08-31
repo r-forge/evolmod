399d0a7caec2530e6df075a7a8e96c4626a074cf
peterchi (Peter Chi) <pchi@calpoly.edu>
2013-08-31 06:40:28 +0000

    bootstrap functions fixed to include options to change calls to codeml and evolver
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/evolmod@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/synDss/R/calc.null.pboot.R
+++ pkg/synDss/R/calc.null.pboot.R
@@ -1,5 +1,5 @@
 calc.null.pboot <-
-function(data,B,l,m,exhaustive=F){
+function(data,B,l,m,exhaustive=F,codeml="codeml",evolver="evolverNSsites 6"){
    distance<-dist.dna(as.DNAbin(data),model="F84")*3
    ols.tree<-nnls.tree(distance,bionj(distance),trace=0)
    data<-as.matrix.alignment(data)
@@ -7,10 +7,10 @@ function(data,B,l,m,exhaustive=F){
    n.taxa<-dim(data)[1]
    Dss.null<-rep(NA,B)
 
-   params<-paml.codeml(data,ols.tree)
+   params<-paml.codeml(data,ols.tree,codeml)
 
    for(i in 1:B){
-      align.taxa<-sim.codon(n.taxa,length.codons,1,ols.tree,freqs=params$freqs,omega=params$omega,kap=params$kap)[[1]]  
+      align.taxa<-sim.codon(n.taxa,length.codons,1,ols.tree,freqs=params$freqs,omega=params$omega,kap=params$kap,evolver)[[1]]  
       data<-t(matrix(as.vector(unlist(apply(align.taxa,1,strsplit,""))),ncol=nrow(align.taxa))) # for calc.dss
       data.tmp<-ape::as.alignment(data)
 
--- pkg/synDss/R/calc.null.pboot.syn.R
+++ pkg/synDss/R/calc.null.pboot.syn.R
@@ -1,14 +1,14 @@
 calc.null.pboot.syn <-
-function(data,B,l,m,syn.matrix,exhaustive=F){
+function(data,B,l,m,syn.matrix,exhaustive=F,codeml="codeml",evolver="evolverNSsites 6"){
    distance<-dist.dna(as.DNAbin(data),model="F84")*3
    ols.tree<-nnls.tree(distance,bionj(distance),trace=0)
-   params<-paml.codeml(as.matrix.alignment(data),ols.tree)   
+   params<-paml.codeml(as.matrix.alignment(data),ols.tree,codeml)   
    data<-make.mj.align(as.matrix.alignment(data))
    length.codons<-dim(data)[2]/3
    n.taxa<-dim(data)[1]
    Dss.null<-rep(NA,B)
    for(i in 1:B){
-      align.taxa<-sim.codon(n.taxa,length.codons,1,ols.tree,freqs=params$freqs,omega=params$omega,kap=params$kap)[[1]]  
+      align.taxa<-sim.codon(n.taxa,length.codons,1,ols.tree,freqs=params$freqs,omega=params$omega,kap=params$kap,evolver)[[1]]  
       data<-t(matrix(as.vector(unlist(apply(align.taxa,1,strsplit,""))),ncol=nrow(align.taxa)))
       data.tmp<-ape::as.alignment(data)
 
