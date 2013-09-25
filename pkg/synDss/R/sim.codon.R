2e1bd12b38227ea028ed51d99339cc65ba278c87
peterchi (Peter Chi) <pchi@calpoly.edu>
2013-09-25 05:59:42 +0000

    updated sim.codon function to include error checking
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/evolmod@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/synDss/R/sim.codon.R
+++ pkg/synDss/R/sim.codon.R
@@ -4,16 +4,30 @@ function(n.taxa,n.codons,reps,tree,freqs=c(0.6,0.3,0.1),omega=c(0.1,0.8,3.2),kap
    # need to add error checking, or else it will create directories and crash before it can remove them
    current.dir<-getwd()
    tmp<-NA
-   while(length(tmp)<(n.taxa+6)){	# fault tolerance
-      temp.dir<-make.dat(n.taxa,n.codons,reps,tree,freqs,omega,kap) # write paml.dat and return the dir it is in
-      setwd(temp.dir)	
-      command<-paste(call,"6 paml.dat")
-      system(command,ignore.stdout=T,show.output.on.console=F)
-      tmp<-readLines("mc.paml")
-      if(length(tmp)<(n.taxa+6)){
-         print("paml error, re-trying evolver")
-      }
+   temp.dir<-make.dat(n.taxa,n.codons,reps,tree,freqs,omega,kap) # write paml.dat and return the dir it is in
+   setwd(temp.dir)	
+   command<-paste(call,"6 paml.dat")
+   err1<-try(system(command,ignore.stdout=T,show.output.on.console=F))
+   if(err1!=0){
+      setwd(current.dir)
+      unlink(temp.dir, recursive=TRUE)
+      stop("error in evolver call")
    }
+   err2<-is.element("mc.paml",list.files())
+   if(!err2){
+      setwd(current.dir)
+      unlink(temp.dir, recursive=TRUE)
+      stop("error in reading mc.paml file")
+   } else {
+      tmp<-readLines("mc.paml")
+   }
+   err3<-length(tmp)<(n.taxa+6)
+   if(err3){
+      setwd(current.dir)
+      unlink(temp.dir, recursive=TRUE)
+      stop("error in mc.paml file")
+   }
+
    starts<-intersect(grep(n.taxa,tmp),grep(n.codons*3,tmp))+1	# the rows that have both n.taxa and n.codons
    all.aligns<-vector("list",reps)
    for(i in 1:reps){
