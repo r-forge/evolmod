fd3bbcbea5d201274d2b4452b3179aef59aac9bc
gimholte (Gregory Imholte) <gimholte@uw.edu>
2013-11-11 20:25:11 +0000

    add one more small test.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/evolmod@122 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/indorigin/inst/tests/test_q_probs.R
+++ pkg/indorigin/inst/tests/test_q_probs.R
@@ -7,8 +7,12 @@ test_that("q probs sum approximately to transition probs",
             n_max = 200
             trans_q = pcltTest(l0, l1, t, n_max)
             
-            q01 = trans[1,2]
+            q01 = trans[1, 2]
             q01_test = sum(exp(trans_q[seq(2, n_max, by = 2)]))
-            expect_equal(q01_test, q01, tolerance = 1e-5,
+            expect_equal(q01_test, q01, tolerance = 1e-6,
                     scale = q01)
+            q00 = trans[1, 1]
+            q00_test = sum(exp(trans_q[seq(1, n_max, by = 2)]))
+            expect_equal(q00_test, q00, tolerance = 1e-6,
+                    scale = q00)
         })
