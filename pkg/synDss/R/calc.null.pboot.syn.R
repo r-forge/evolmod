4f09b9bd24a5f20bc0cca5f3373e043ebba055d0
peterchi (Peter Chi) <pchi@calpoly.edu>
2013-12-12 22:18:52 +0000

    Update parametric bootstrap functions to fix taxa names as needed.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/evolmod@124 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/synDss/R/calc.null.pboot.syn.R
+++ pkg/synDss/R/calc.null.pboot.syn.R
@@ -1,5 +1,13 @@
 calc.null.pboot.syn <-
 function(data,B,l,m,syn.matrix,exhaustive=F,codeml="codeml",evolver="evolverNSsites"){
+
+   IDnum<-which(!is.na(suppressWarnings(as.numeric(substring(data$nam,1,1)))))
+   if(length(IDnum)>0){
+      for(i in 1:length(IDnum)){
+         data$nam[IDnum[i]]<-paste("N",data$nam[IDnum[i]],sep="")
+      }
+   }		# append "N" in front of taxa names that start with a number; codeml tanks otherwise
+
    distance<-dist.dna(as.DNAbin(data),model="F84")*3
    ols.tree<-nnls.tree(distance,bionj(distance),trace=0)
    params<-paml.codeml(data,ols.tree,codeml)   
